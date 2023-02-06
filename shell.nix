2da9a66636e6ba7748e8608525223be7cadf51ac
Konrad Malik <konrad.malik@gmail.com>
2023-02-06 12:13:07 +0100

    ci: actions

---
+++ shell.nix
@@ -0,0 +1,17 @@
+let
+  flakeLock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  nixpkgsRev = flakeLock.nodes.nixpkgs.locked.rev;
+  lockedNixpkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/${nixpkgsRev}.tar.gz") { };
+in
+{ pkgs ? lockedNixpkgs }:
+pkgs.mkShell {
+  name = "page-shell";
+
+  nativeBuildInputs = with pkgs; [
+    # nix
+    nil
+    nixpkgs-fmt
+    #
+    hugo
+  ];
+}
