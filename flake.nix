e5954c3aee37fded5aa3d4be22ebd933ad56953f
Konrad Malik <konrad.malik@gmail.com>
2024-10-06 22:29:36 +0200

    chore(deps): update

--- flake.nix
+++ flake.nix
@@ -2,35 +2,41 @@
   description = "Personal page";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs =
+    { nixpkgs, ... }:
     let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ]
-          (system:
-            function (import nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            }));
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ]
+          (
+            system:
+            function (
+              import nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
+              }
+            )
+          );
     in
     {
       devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell
-          {
-            name = "page";
+        default = pkgs.mkShell {
+          name = "page";
 
-            packages = with pkgs; [
-              hugo
-              vale
-            ];
-          };
+          packages = with pkgs; [
+            hugo
+            vale
+          ];
+        };
       });
       formatter = forAllSystems (pkgs: pkgs.nixpkgs-fmt);
     };
